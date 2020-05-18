573ce56341a1250aeddcaf7e62aeb476cf9ae26e
Tom Richard <tom@probiz-integrasi.com>
2020-05-18 11:50:15 +0700

    update windows - linux cli

--- app/web/components/pusaka/combo.easyui.php
+++
@@ -1,4 +0,0 @@
-@component:pusaka.alert
-	@id = "1"
-	@e 	= 2
-@endcomponent;
--- app/web/components/pusaka/combo.ui.php
+++
@@ -1,1 +0,0 @@
-<?php $component5EC1DD0713C2C = function($id,$e) { ?>				<?php // component : pusaka.alert ?>				<alert>	<?php foreach( $users as $user ) : ?>				<?php echo $user ?>	<?php endforeach; ?></alert>			<?php }; $component5EC1DD0713C2C("1",2); ?>
